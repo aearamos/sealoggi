b2805629d5e356b19a05237b0eacf6fb93d3c341
Arthur Ramos <ramos.oceano@gmail.com>
2017-09-22 16:20:46 -0300

    some validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ApplicationRecord
   has_many :cruises
   has_many :equipments
   has_many :data
+  validates :description, presence: :true
+  validates :name, presence: :true
+  validates :responsible, presence: :true
 end
+
+
+
+
+
